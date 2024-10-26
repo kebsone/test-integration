ba5c25d82f1967fdbbf0c678740a457b27138ad5
kebsone <kebeseugne@gmail.com>
2024-10-26 10:54:46 +0200

    first commit

---
+++ home-2/home-2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-2',
+  templateUrl: './home-2.component.html',
+  styleUrl: './home-2.component.scss'
+})
+export class Home2Component {
+
+}
---
+++ home-3/home-3.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-3',
+  templateUrl: './home-3.component.html',
+  styleUrl: './home-3.component.scss'
+})
+export class Home3Component {
+
+}
